73fd46d7e2c8682f9253f004baf19c602de46b1f
rosalinda7-dot <rodriguezmostacedorosalinda7@gmail.com>
2025-10-26 09:11:51 -0400

    agregue segundo tiempo

--- calculadoraip/hola.java
+++ calculadoraip/hola.java
@@ -2,6 +2,7 @@ package calculadoraip;
 
 public class hola {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Ho");
+        System.out.println("papa");
 }
  }
